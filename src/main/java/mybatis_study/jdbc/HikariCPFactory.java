de6d8f10a245702179fb7f1c70176a6096590610
Sunny Park <dlstjs8246@naver.com>
2020-03-30 12:14:16 +0900

    git init

---
+++ src/main/java/mybatis_study/jdbc/HikariCPFactory.java
@@ -0,0 +1,30 @@
+package mybatis_study.jdbc;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.ibatis.datasource.unpooled.UnpooledDataSourceFactory;
+import org.apache.ibatis.io.Resources;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class HikariCPFactory extends UnpooledDataSourceFactory {
+	private HikariDataSource dataSource;
+	private int minIdle = 10;
+	private int maxPoolSize = 100;
+	public HikariCPFactory() {
+		try {
+			Properties prop = Resources.getResourceAsProperties("application.properties");
+			HikariConfig cfg = new HikariConfig(prop);
+			dataSource = new HikariDataSource(cfg);
+			dataSource.setMinimumIdle(minIdle);
+			dataSource.setMaximumPoolSize(maxPoolSize);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public HikariDataSource getDataSource() {
+		return dataSource;
+	}
+}
